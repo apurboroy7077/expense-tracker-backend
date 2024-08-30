646032a1790c1daf59104465471a8b213885f455
Apurbo Roy <apurboroy7077@gmail.com>
2024-08-30 18:51:24 +0600

    Added refining function

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import server from "./app";
 import { SOCKET_SERVER_PORT } from "./data/quiz-game/environment-variable";
+import refineGamingData from "./socket/functions/refine-gaming-data/refineGamingData";
 
 server.listen(SOCKET_SERVER_PORT, () => {
   console.log(`Socket is running on http://localhost:${SOCKET_SERVER_PORT}`);
+  setInterval(() => {
+    refineGamingData();
+  }, 1000);
 });
