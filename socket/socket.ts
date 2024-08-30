4b0446dd36921e4d04274976ce8f734fb269dda8
Apurbo Roy <apurboroy7077@gmail.com>
2024-08-30 17:41:54 +0600

    trying online mode

--- socket/socket.ts
+++ socket/socket.ts
@@ -25,5 +25,5 @@ httpServer.listen(port, () => {
   console.log(`Socket Server is started at http://localhost:${port}`);
   setInterval(() => {
     refineGamingData();
-  }, 3000);
+  }, 1000);
 });
